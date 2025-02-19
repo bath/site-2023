e91cc3190a26b32cce44eb422b7cc3c16c45de33
Miller Bath <bath@MacBook-Pro-5.local>
2025-02-19 17:36:47 -0600

    still broken

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static", // Explicitly set to static output
   site: "https://mzb.dev", // Replace with your actual domain
+  build: {
+    format: "directory",
+  },
 });
