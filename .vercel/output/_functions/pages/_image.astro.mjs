500b14ea48a029e115bae03e35581c3ae2ff2675
Miller Bath <bath@MacBook-Pro-5.local>
2025-02-19 18:07:59 -0600

    rm that stuff

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DRA4epi5.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: "server",
-  site: "https://mzb.dev", // Replace with your actual domain
-  build: {
-    format: "directory",
-  },
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
-    },
-    devMode: process.env.NODE_ENV === "development",
-    functionPerRoute: true,
-    maxDuration: 10,
-    debug: true,
-  }),
   vite: {
     build: {
       minify: false,
